f741c37e62cae1dff42c3dc81605bb7425450727
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-13 22:39:09 +0200

    Update 4.Water_dispenser.py

--- 1.Lists_as_Stacks_and_Queues/4.Water_dispenser.py
+++ 1.Lists_as_Stacks_and_Queues/4.Water_dispenser.py
@@ -26,3 +26,40 @@ while True:
             print(f"{person} must wait")
 
 print(f"{current_quantity} liters left")
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that keeps track of people getting water from a dispenser and the amount of water
+# left at the end. On the first line, you will receive the starting quantity of water (integer) in a
+# dispenser. Then, on the following lines, you will be given the names of some people who want to get
+# water (each on a separate line) until you receive the command "Start". Add those people in a queue.
+# Finally, you will receive some commands until the command "End":
+#   * "{liters}" - litters (integer) that the current person in the queue wants to get. Check if there
+#     is enough water in the dispenser for that person. If there is enough water, print "{person_name}
+#     got water" and remove him/her from the queue. Otherwise, print "{person_name} must wait" and
+#     remove the person from the queue without reducing the water in the dispenser.
+#   * "refill {liters}" - add the given litters in the dispenser.
+# In the end, print how many liters of water have left in the format: "{left_liters} liters left".
+# -------------------------------------- Example inputs ----------------------------------
+# Input	            Output
+# 2                 Peter got water
+# Peter             Amy got water
+# Amy               0 liters left
+# Start
+# 2
+# refill 1
+# 1
+# End
+# -----------------------------------
+# 10                Peter got water
+# Peter             George got water
+# George            Amy got water
+# Amy               Alice must wait
+# Alice             2 liters left
+# Start
+# 2
+# 3
+# 3
+# 3
+# End
