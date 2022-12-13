9a8939b3cbd4ce827b7fc975b38da93859516a0e
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-13 21:27:07 +0200

    Update 2.Matching_parentheses.py

--- 1.Lists_as_Stacks_and_Queues/2.Matching_parentheses.py
+++ 1.Lists_as_Stacks_and_Queues/2.Matching_parentheses.py
@@ -6,6 +6,24 @@ for index in range(len(text)):
     if char == "(":
         stack.append(index)
     elif char == ")":
-        last_opening_bracket = stack.pop()
-        parentheses = text[last_opening_bracket:index + 1]
+        start_bracket = stack.pop()
+        parentheses = text[start_bracket:index + 1]
         print(parentheses)
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You are given an algebraic expression with parentheses.
+# Scan through the string and extract each set of parentheses.
+#   * If you find an opening parenthesis, push the index into the stack.
+#   * If you find a closing parenthesis, pop the topmost element from the stack. This is the index
+#   of the last opening parenthesis.
+#   * Use the current index and the popped one to extract a set of parentheses.
+# Print the result back on the console.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	Output
+# 1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5	(2 + 3)
+# (3 + 1)
+# (2 - (2 + 3) * 4 / (3 + 1))
+# (2 + 3) - (2 + 3)	(2 + 3)
+# (2 + 3)
