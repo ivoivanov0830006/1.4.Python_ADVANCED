f768abecce412727a9ff8a2388077222e74fc173
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-13 21:27:58 +0200

    Update 1.Reverse_strings.py

--- 1.Lists_as_Stacks_and_Queues/1.Reverse_strings.py
+++ 1.Lists_as_Stacks_and_Queues/1.Reverse_strings.py
@@ -1,7 +1,20 @@
 text = list(input())
 stack = []
 
-for i in range(len(text)):
-    stack.append(text.pop())
+for i in range(len(text)):          # while text:
+    stack.append(text.pop())        
 
 print("".join(stack))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write program that:
+#   * Reads an input string
+#   * Reverses it using a stack
+# Prints the result back on the console
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                            Output
+# I Love Python	                    nohtyP evoL I
+# ------------------------------------------------
+# Stacks and Queues	                seueuQ dna skcatS
