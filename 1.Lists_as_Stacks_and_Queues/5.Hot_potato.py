6f64d7a93ea8fef966e2c540f1b45ff96b1ff0d4
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-19 19:43:16 +0200

    Update 5.Hot_potato.py

--- 1.Lists_as_Stacks_and_Queues/5.Hot_potato.py
+++ 1.Lists_as_Stacks_and_Queues/5.Hot_potato.py
@@ -32,3 +32,32 @@ print(f"Last is {last_child}")
 #
 # last_child = children.popleft()
 # print(f"Last is {last_child}")
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Hot Potato is a game in which children form a circle and toss a hot potato. The counting starts with
+# the first kid. Every nth toss, the child holding the potato leaves the game. When a kid leaves the
+# game, it passes the potato to the next kid. It continues until there is only one kid left.
+# Create a program that simulates the game of Hot Potato. On the first line, you will receive kids'
+# names, separated by a single space. On the second line, you will receive the nth toss (integer) in
+# which a child leaves the game.
+# Print every kid who is removed from the circle in the format "Removed {kid}". In the end, print the
+# only kid left in the format "Last is {kid}".
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                                    Output
+# Tracy Emily Daniel                        Removed Emily
+# 2	                                        Removed Tracy
+#                                           Last is Daniel
+# --------------------------------------------------------
+# George Peter Michael William Thomas       Removed Thomas
+# 10                                        Removed Peter
+#                                           Removed Michael
+#                                           Removed George
+#                                           Last is William
+# ---------------------------------------------------------
+# George Peter Michael William Thomas       Removed George
+# 1	                                        Removed Peter
+#                                           Removed Michael
+#                                           Removed William
+#                                           Last is Thomas
