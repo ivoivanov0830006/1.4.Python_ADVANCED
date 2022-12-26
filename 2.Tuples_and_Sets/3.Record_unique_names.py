071fa44a7c85b58667087ae68360587f493c392d
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-26 23:45:53 +0200

    Update 3.Record_unique_names.py

--- 2.Tuples_and_Sets/3.Record_unique_names.py
+++ 2.Tuples_and_Sets/3.Record_unique_names.py
@@ -6,3 +6,33 @@ for _ in range(number_names):
     unique_names.add(name)
 
 print("\n".join(unique_names))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program, which will take a list of names and print only the unique names in the list.
+# The order in which we print the result does not matter.
+# -------------------------------------- Example inputs ----------------------------------
+# Input	            Output
+# 8                 Alan
+# Lee               Joey
+# Joey              Lee
+# Lee               Joe
+# Joe               Peter
+# Alan
+# Alan
+# Peter
+# Joey
+# ------------------------------------------------------
+# 7                 Easton
+# Lyle              Lyle
+# Bruce             Alice
+# Alice             Bruce
+# Easton            Shawn
+# Shawn
+# Alice
+# Shawn
+# -------------------------------------------------------
+# 6                 Adam
+# Adam
+
