0320c13f249f22b74b5d053d139a218b744b8423
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-28 17:57:52 +0200

    Update *6.Battle_of_names.py

--- 2.Tuples_and_Sets/*6.Battle_of_names.py
+++ 2.Tuples_and_Sets/*6.Battle_of_names.py
@@ -30,3 +30,32 @@ else:
 
 result = (', '.join(str(x) for x in result))
 print(result)
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# You will receive a number N. On the following N lines, you will be receiving names. You should sum the ASCII
+# values of each letter in the name and integer divide it by the number of the current row (starting from 1).
+# Save the result to a set of either odd or even numbers, depending on if the resulting number is odd or even.
+# After that, sum the values of each set.
+#   * If the sums of the two sets are equal, print the union of the values, separated by ", ".
+#   * If the sum of the odd numbers is bigger than the sum of the even numbers, print the different values,
+#   separated by ", ".
+#   * If the sum of the even numbers is bigger than the sum of the odd numbers, print the symmetric-different
+#   values, separated by ", ".
+# NOTE: On every operation, the starting set should be the odd set
+# -------------------------------------- Example inputs ----------------------------------
+# Input	            Output
+# 4                 304, 128, 206, 511
+# Pesho
+# Stefan
+# Stamat
+# Gosho
+# ---------------------------------------
+# 6                 733, 101
+# Preslav
+# Gosho
+# Ivan
+# Stamat
+# Pesho
+# Stefan
