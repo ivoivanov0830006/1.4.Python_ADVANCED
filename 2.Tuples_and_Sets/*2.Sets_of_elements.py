6d422ae14ab2e0dd6c807278e8c5f4071c396263
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-28 17:54:12 +0200

    Update *2.Sets_of_elements.py

--- 2.Tuples_and_Sets/*2.Sets_of_elements.py
+++ 2.Tuples_and_Sets/*2.Sets_of_elements.py
@@ -15,3 +15,32 @@ for _ in range(m):
 
 unique_numbers = n_numbers.intersection(m_numbers)
 print("\n".join(unique_numbers))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that prints a set of elements. On the first line, you will receive two numbers - n and m,
+# separated by a single space - representing the lengths of two separate sets. On the next n + m lines,
+# you will receive n numbers, which are the numbers in the first set, and m numbers, which are in the
+# second set. Find all the unique elements that appear in both and print them on separate lines
+# (the order does not matter).
+# For example:
+# Set with length n = 4: {1, 3, 5, 7}
+# Set with length m = 3: {3, 4, 5}
+# Set that contains all the elements that repeat in both sets -> {3, 5}
+# -------------------------------------- Example inputs ----------------------------------
+# Input	        Output
+# 4 3           3
+# 1             5
+# 3
+# 5
+# 7
+# 3
+# 4
+# -----------------------
+# 5             1
+# 2 2
+# 1
+# 3
+# 1
+# 5
