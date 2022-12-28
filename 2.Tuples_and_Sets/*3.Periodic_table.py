07df4e544d6c6f33dbc27b01987e28be29e857f2
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-28 17:55:33 +0200

    Update *3.Periodic_table.py

--- 2.Tuples_and_Sets/*3.Periodic_table.py
+++ 2.Tuples_and_Sets/*3.Periodic_table.py
@@ -6,3 +6,29 @@ for _ in range(number_elements):
     unique_elements = unique_elements.union(compound)
 
 print("\n".join(unique_elements))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that keeps all the unique chemical elements. On the first line, you will be given a
+# number n - the count of input lines that you will receive. On the following n lines, you will be
+# receiving chemical compounds separated by a single space. Your task is to print all the unique ones on
+# separate lines (the order does not matter):
+# -------------------------------------- Example inputs ----------------------------------
+# Input	                Output
+# 4                     Ce
+# Ce O                  Ee
+# Mo O Ce               Mo
+# Ee                    O
+# Mo
+# ---------------------------------
+# 3
+# Ge Ch O Ne
+# Nb Mo Tc
+# O Ne	Ch
+# Ge
+# Mo
+# Nb
+# Ne
+# O
+# Tc
