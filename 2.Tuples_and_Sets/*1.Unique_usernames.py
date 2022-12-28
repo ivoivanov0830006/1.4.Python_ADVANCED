5aaff1a427a2d2279e051e33fea8bbceb32e18c4
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-28 17:53:17 +0200

    Update *1.Unique_usernames.py

--- 2.Tuples_and_Sets/*1.Unique_usernames.py
+++ 2.Tuples_and_Sets/*1.Unique_usernames.py
@@ -6,3 +6,31 @@ for _ in range(number_usernames):
     unique_usernames.add(current_name)
 
 print("\n".join(unique_usernames))
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that reads from the console a sequence of N usernames and keeps a collection only of the
+# unique ones. On the first line, you will receive an integer N. On the next N lines, you will receive a
+# username. Print the collection on the console (the order does not matter):
+# -------------------------------------- Example inputs ----------------------------------
+# Input	            Output
+# 6                 George
+# George            Peter
+# George            NiceGuy1234
+# George
+# Peter
+# George
+# NiceGuy1234
+# ----------------------------------
+# 10                Peter
+# Peter             Maria
+# Maria             George
+# Peter             Steve
+# George            Alex
+# Steve
+# Maria
+# Alex
+# Peter
+# Steve
+# George
