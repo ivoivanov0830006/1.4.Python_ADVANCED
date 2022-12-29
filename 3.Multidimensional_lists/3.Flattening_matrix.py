ae894b7d0c6cde6147e5b173a45eeab8adda3ed3
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-29 19:03:56 +0200

    Update 3.Flattening_matrix.py

--- 3.Multidimensional_lists/3.Flattening_matrix.py
+++ 3.Multidimensional_lists/3.Flattening_matrix.py
@@ -6,3 +6,29 @@ for _ in range(rows):
     for number in row:
         flattened_matrix.append(number)
 print(flattened_matrix)
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Write a program that receives a matrix and prints the flattened version of it (a list with all the 
+values). For example, the flattened list of the matrix: [[1, 2], [3, 4]] will be [1, 2, 3, 4].
+On the first line, you will receive the number of a matrix's rows. On the next rows, you will get the 
+elements for each column separated with a comma and a space ", ".
+-------------------------------------- Example inputs ----------------------------------
+Input	
+2
+1, 2, 3
+4, 5, 6	
+Output
+[1, 2, 3, 4, 5, 6]
+---------------------
+Input	
+3
+10, 2, 21, 4
+5, 20, 41, 9
+6, 2, 4, 99	
+Output
+[10, 2, 21, 4, 5, 20, 41, 9, 6, 2, 4, 99]
+
+"""
