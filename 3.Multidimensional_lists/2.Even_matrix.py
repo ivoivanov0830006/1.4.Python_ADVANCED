d4837b44e1176ac7a2445a26508aed3c8fa9463c
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-29 18:46:58 +0200

    Update 2.Even_matrix.py

--- 3.Multidimensional_lists/2.Even_matrix.py
+++ 3.Multidimensional_lists/2.Even_matrix.py
@@ -9,3 +9,29 @@ for _ in range(rows):
             new_row.append(number)
     matrix.append(new_row)
 print(matrix)
+
+
+"""
+------------------------------------- Problem to resolve ------------------------------
+
+Write a program that receives a matrix of numbers and prints a new one only with the numbers that
+are even. Use nested comprehension for that problem.
+On the first line, you will receive the rows of the matrix. On the next rows, you will get elements
+for each column separated with a comma and a space ", ".
+-------------------------------------- Example inputs ----------------------------------
+Input
+2
+1, 2, 3
+4, 5, 6
+Output
+[[2], [4, 6]]
+---------------------------------
+Input
+4
+10, 33, 24, 5, 1
+67, 34, 11, 110, 3
+4, 12, 33, 63, 21
+557, 45, 23, 55, 67
+Output
+[[10, 24], [34, 110], [4, 12], []]
+"""
