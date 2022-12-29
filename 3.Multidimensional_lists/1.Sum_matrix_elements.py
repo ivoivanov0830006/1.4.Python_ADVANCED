96c679cef6f7246ad122a5d0f775f484b7abc9bd
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2022-12-29 18:40:58 +0200

    Update 1.Sum_matrix_elements.py

--- 3.Multidimensional_lists/1.Sum_matrix_elements.py
+++ 3.Multidimensional_lists/1.Sum_matrix_elements.py
@@ -9,3 +9,21 @@ for _ in range(rows):
 
 print(total_sum)
 print(matrix)
+
+
+# ------------------------------------- Problem to resolve ------------------------------
+#
+# Write a program that reads a matrix from the console and prints:
+# ⦁	The sum of all numbers in the matrix
+# ⦁	The matrix itself
+# On the first line, you will receive the matrix sizes in the format "{rows}, {columns}".
+# On the next rows, you will get elements for each column separated by a comma and a space ", ".
+# -------------------------------------- Example inputs ----------------------------------
+# Input
+# 3, 6
+# 7, 1, 3, 3, 2, 1
+# 1, 3, 9, 8, 5, 6
+# 4, 6, 7, 9, 1, 0
+# Output
+# 76
+# [[7, 1, 3, 3, 2, 1], [1, 3, 9, 8, 5, 6], [4, 6, 7, 9, 1, 0]]
