d05d376dcf9ca70a66f7b581a99aa088ea27d929
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-01 22:19:39 +0200

    Update 5.Primary_diagonal.py

--- 3.Multidimensional_lists/5.Primary_diagonal.py
+++ 3.Multidimensional_lists/5.Primary_diagonal.py
@@ -11,3 +11,45 @@ for _ in range(n):
     matrix.append(row)
 
 print(get_primary_diagonal(matrix))
+
+
+"""
+------------------------------------- Another Solution ---------------------------------
+
+# def get_primary_diagonal(current_matrix):
+#     the_sum = 0
+#     for i in range(len(matrix)):
+#         the_sum += matrix[i][i]
+#     return the_sum
+#
+#
+# n = int(input())
+#
+# matrix = []
+# for _ in range(n):
+#     row = [int(x) for x in input().split(" ")]
+#     matrix.append(row)
+#
+# print(get_primary_diagonal(matrix))
+
+------------------------------------ Problem to resolve --------------------------------
+
+
+-------------------------------------- Example inputs ----------------------------------
+Input	
+3
+11 2 4
+4 5 6
+10 8 -12	
+Output
+4	
+-------------------
+Input
+3
+1 2 3
+4 5 6
+7 8 9
+Output
+15
+
+"""
