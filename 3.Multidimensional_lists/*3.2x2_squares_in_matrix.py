4adecf391b0c67e8442347bb063e615769b3f133
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-02 20:44:13 +0200

    Update *3.2x2_squares_in_matrix.py

--- 3.Multidimensional_lists/*3.2x2_squares_in_matrix.py
+++ 3.Multidimensional_lists/*3.2x2_squares_in_matrix.py
@@ -10,3 +10,38 @@ for r in range(len(matrix) - 1):
             count += 1
 
 print(count)
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Find the number of all 2x2 squares containing identical chars in a matrix. On the first line, you will 
+receive the matrix's dimensions in the format "{rows} {columns}". On the following rows, you will receive
+characters separated by a single space. Print the number of all square matrices you have found.
+-------------------------------------- Example inputs ----------------------------------
+Input	
+3 4
+A B B D
+E B B B
+I J B B	
+Output
+2
+----------------------
+Input
+2 2
+a b
+c d
+Output
+0
+---------------------
+Input
+5 4
+A A B D
+A A B B
+I J B B
+C C C G
+C C K P	
+Output
+3
+
+"""
