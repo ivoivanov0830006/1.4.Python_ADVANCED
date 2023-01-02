9dcc85782672760101d154537986dda68262cd82
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-02 20:41:40 +0200

    Update *1.Diagonals.py

--- 3.Multidimensional_lists/*1.Diagonals.py
+++ 3.Multidimensional_lists/*1.Diagonals.py
@@ -16,3 +16,24 @@ secondary_diagonal_numbers = (", ".join(map(str, secondary_diagonal)))
 
 print(f'Primary diagonal: {primary_diagonal_numbers}. Sum: {sum(primary_diagonal)}')
 print(f'Secondary diagonal: {secondary_diagonal_numbers}. Sum: {sum(secondary_diagonal)}')
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Using a nested list comprehension, write a program that reads rows of a square matrix and its elements, 
+separated by a comma and a space ", ". You should find the matrix's diagonals, prints them and their sum 
+in the format:
+    "Primary diagonal: {element1}, {element2}, … {elementN}. Sum: {sum_of_primary}
+    Secondary diagonal: {element1}, {element2}, … {elementN}. Sum: {sum_of_secondary}".
+-------------------------------------- Example inputs ----------------------------------
+Input	
+3
+1, 2, 3
+4, 5, 6
+7, 8, 9	
+Output
+Primary diagonal: 1, 5, 9. Sum: 15
+Secondary diagonal: 3, 5, 7. Sum: 15
+
+"""
