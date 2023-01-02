569048e7b4c9b7b67355ba93493cd24f0d666cce
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-02 20:42:59 +0200

    Update *2.Diagonal_difference.py

--- 3.Multidimensional_lists/*2.Diagonal_difference.py
+++ 3.Multidimensional_lists/*2.Diagonal_difference.py
@@ -18,3 +18,35 @@ for row_index in range(len(matrix)):
 
 diff = abs(sum(primary_diagonal) - sum(secondary_diagonal))
 print(diff)
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Write a program that finds the difference between the sums of the square matrix diagonals (absolute value).
+On the first line, you will receive an integer N - the size of a square matrix. The following N lines hold
+the values for each column - N numbers separated by a single space. Print the absolute difference between 
+the primary and the secondary diagonal sums.
+-------------------------------------- Example inputs ----------------------------------
+Input
+3
+11 2 4
+4 5 6
+10 8 -12	
+Output
+15	
+Comments
+Primary diagonal: sum = 11 + 5 + (-12) = 4
+Secondary diagonal: sum = 4 + 5 + 10 = 19
+Difference: |4 - 19| = 15
+-------------------------------------------------
+Input
+4
+-7 14 9 -20
+3 4 9 21
+-14 6 8 44
+30 9 7 -14	
+Output
+34	
+
+"""
