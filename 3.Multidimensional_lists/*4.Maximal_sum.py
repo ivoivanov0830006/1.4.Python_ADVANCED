c351aa54c0f0f1ac8c09c2bb334870b25ec5f418
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-06 22:30:17 +0200

    Update and rename 4.Maximal_sum.py to *4.Maximal_sum.py

--- 3.Multidimensional_lists/*4.Maximal_sum.py
+++ 3.Multidimensional_lists/*4.Maximal_sum.py
@@ -20,3 +20,34 @@ for r in range(len(matrix) - 2):
 print(f"Sum = {max_sum}")
 for row in max_sub_matrix:
     print(*row, sep=" ")
+
+
+"""
+Write a program that reads a rectangular matrix's dimensions and finds the 3x3square with a maximum sum of its elements. There will be no case with two or more 3x3 squares with equal maximal sum.
+Input
+⦁	On the first line, you will receive the rows and columns in the format "{rows} {columns}" – integers in the range [1, 20]
+⦁	On the following lines, you will receive each row with its columns - integers, separated by a single space in the range [-20, 20]
+Output
+⦁	On the first line, print the maximum sum of the elements in the 3x3 square in the format "Sum = {sum}"
+⦁	On the following 3 lines, print each element of the found submatrix, separated by a single space
+Examples
+Input	Output
+4 5
+1 5 5 2 4
+2 1 4 14 3
+3 7 11 2 8
+4 8 12 16 4	 	Sum = 75
+1 4 14
+7 11 2
+8 12 16
+5 6
+1 0 4 3 1 1
+1 3 1 3 0 4
+6 4 1 2 5 6
+2 2 1 5 4 1
+3 3 3 6 0 5		Sum = 34
+2 5 6 
+5 4 1 
+6 0 5
+
+"""
