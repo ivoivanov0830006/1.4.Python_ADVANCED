9c537dec0037877e7f0789eb1554a24d9d434e6e
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-06 22:31:07 +0200

    Update *6.Matrix_shuffling.py

--- 3.Multidimensional_lists/*6.Matrix_shuffling.py
+++ 3.Multidimensional_lists/*6.Matrix_shuffling.py
@@ -25,3 +25,32 @@ while True:
 
     for row in matrix:
         print(*row, sep=" ")
+
+        
+------------------------------------- Another Solution ---------------------------------
+def valid(current_command):
+    row1, col1, row2, col2 = [x for x in command[1:]]
+    if row1.isdigit() and col1.isdigit() and row2.isdigit() and col2.isdigit():
+        if int(row1) < rows and int(row2) < rows and int(col1) < columns and int(col2) < columns:
+            return True
+    return False
+
+
+rows, columns = [int(x) for x in input().split(' ')]
+matrix = [input().split(' ') for _ in range(rows)]
+
+while True:
+    command = input().split()
+    if command[0] == "END":
+        break
+    action = command[0]
+    if action == "swap" and len(command) == 5 and valid(command):
+        r1 = int(command[1])
+        c1 = int(command[2])
+        r2 = int(command[3])
+        c2 = int(command[4])
+        matrix[r1][c1], matrix[r2][c2] = matrix[r2][c2], matrix[r1][c1]
+        for row in matrix:
+            print(*row, sep=" ")
+    else:
+        print("Invalid input!")
