1ecde3a5130d2a62770980da1f86d215df67cea6
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-06 22:31:57 +0200

    Update *6.Matrix_shuffling.py

--- 3.Multidimensional_lists/*6.Matrix_shuffling.py
+++ 3.Multidimensional_lists/*6.Matrix_shuffling.py
@@ -27,6 +27,7 @@ while True:
         print(*row, sep=" ")
 
         
+"""
 ------------------------------------- Another Solution ---------------------------------
 def valid(current_command):
     row1, col1, row2, col2 = [x for x in command[1:]]
@@ -54,3 +55,48 @@ while True:
             print(*row, sep=" ")
     else:
         print("Invalid input!")
+        
+        
+------------------------------------ Problem to resolve --------------------------------
+
+Write a program that reads a matrix from the console and performs certain operations with its elements. 
+User input is provided similarly to the problems above - first, you read the dimensions and then the data. 
+Your program should receive commands in the format: "swap {row1} {col1} {row2} {col2}" where ("row1", 
+"col1") and ("row2", "col2") are the coordinates of two points in the matrix. A valid command starts with 
+the "swap" keyword along with four valid coordinates (no more, no less), separated by a single space.
+⦁	If the command is valid, you should swap the values at the given indexes and print the matrix at each 
+step (thus, you will be able to check if the operation was performed correctly). 
+⦁	If the command is not valid (does not contain the keyword "swap", has fewer or more coordinates entered,
+or the given coordinates are not valid), print "Invalid input!" and move on to the following command. 
+A negative value makes the coordinates not valid.
+Your program should finish when the command "END" is entered.
+-------------------------------------- Example inputs ----------------------------------
+Input	
+2 3
+1 2 3
+4 5 6
+swap 0 0 1 1
+swap 10 9 8 7
+swap 0 1 1 0
+END	
+Output
+5 2 3
+4 1 6
+Invalid input!
+5 4 3
+2 1 6
+-----------------
+Input	
+1 2
+Hello World
+0 0 0 1
+swap 0 0 0 1
+swap 0 1 0 0
+END	
+Output
+Invalid input!
+World Hello
+Hello World
+
+"""
+
