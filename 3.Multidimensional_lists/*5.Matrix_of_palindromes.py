7ca05a366c6b2581ed32b1480482f267c8cc4099
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-06 22:29:22 +0200

    Update *5.Matrix_of_palindromes.py

--- 3.Multidimensional_lists/*5.Matrix_of_palindromes.py
+++ 3.Multidimensional_lists/*5.Matrix_of_palindromes.py
@@ -12,6 +12,9 @@ for row in matrix:
     print(*row)
 
     
+"""
+------------------------------------- Another Solution ---------------------------------
+
 rows, columns = list(map(int, input().split()))
 
 for r in range(97, rows + 97):
@@ -19,3 +22,33 @@ for r in range(97, rows + 97):
         palindrome = f'{chr(r)}{chr(r + c)}{chr(r)}'
         print(palindrome, end=' ')
     print()
+    
+
+------------------------------------ Problem to resolve --------------------------------
+
+Write a program to generate the following matrix of palindromes of 3 letters with r rows and c columns like
+the one in the examples below.
+⦁	Rows define the first and the last letter: row 0  'a', row 1  'b', row 2  'c', …
+⦁	Columns + rows define the middle letter: 
+⦁	column 0, row 0  'a', column 1, row 0  'b', column 2, row 0  'c', …
+⦁	column 0, row 1  'b', column 1, row 1  'c', column 2, row 1  'd', …
+Input
+⦁	The numbers r and c stay at the first line at the input in the format "{rows} {columns}"
+⦁	r and c are integers in the range [1, 26]
+-------------------------------------- Example inputs ----------------------------------
+Input	
+4 6	
+Output
+aaa aba aca ada aea afa
+bbb bcb bdb beb bfb bgb
+ccc cdc cec cfc cgc chc
+ddd ded dfd dgd dhd did
+----------------------------
+Input
+3 2
+Output
+aaa aba
+bbb bcb
+ccc cdc
+
+"""
