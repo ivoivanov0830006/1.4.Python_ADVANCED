d1fdbdd9c339abdbda30752531ffb17332f0b388
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-07 22:26:24 +0200

    Update 2.Person_info.py

--- 4.1.Functions_advanced/2.Person_info.py
+++ 4.1.Functions_advanced/2.Person_info.py
@@ -7,3 +7,18 @@ print(get_info(**{
     "town": "Sofia",
     "age": 20
 }))
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Write a function called get_info that receives a name, an age, and a town and returns a 
+string in the format: 
+"This is {name} from {town} and he is {age} years old". 
+Use dictionary unpacking when testing your function. Submit only the function in the judge system.
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+print(get_info(**{"name": "George", "town": "Sofia", "age": 20}))	
+Output
+This is George from Sofia and he is 20 years old
+"""
