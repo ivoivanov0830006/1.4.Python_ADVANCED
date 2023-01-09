ea673a7061746d04ee516625e7a16535c0e44b1e
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-09 22:32:09 +0200

    Create *4.Numbers_filter.py

---
+++ 4.Functions_advanced/*4.Numbers_filter.py
@@ -0,0 +1,14 @@
+def even_odd_filter(**kwargs):
+    result = {}
+
+    for key, value in kwargs.items():
+        checking = 0 if key == "even" else 1
+        filtered = [x for x in value if x % 2 == checking]
+        result[key] = filtered
+
+    return {key: value for key, value in sorted(result.items(), key=lambda x: -len(x[1]))}
+    # -len(x[1]) is giving descending order
+
+
+print(even_odd_filter(odd=[1, 2, 3, 4, 10, 5], even=[3, 4, 5, 7, 10, 2, 5, 5, 2],))
+print(even_odd_filter(odd=[2, 2, 30, 44, 10, 5],))
