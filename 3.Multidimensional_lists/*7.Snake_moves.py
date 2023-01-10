0ec847a68ef011d40cb6c477c17fc499c4138590
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-10 23:16:30 +0200

    Update *7.Snake_moves.py

--- 3.Multidimensional_lists/*7.Snake_moves.py
+++ 3.Multidimensional_lists/*7.Snake_moves.py
@@ -9,3 +9,23 @@ for row in range(rows):
         elements[col] = word[idx % len(word)]
         idx += 1
     print("".join(elements))
+
+
+"""
+------------------------------------- Another Solution ---------------------------------
+
+rows, cols = [int(x) for x in input().split()]
+word = input()
+
+idx = 0
+for row in range(rows):
+    elements = [None] * cols
+    if row % 2 == 0:
+        for col in range(cols):
+            elements[col] = word[idx % len(word)]  # will work if idx > len(word) because 5 % 7 = 5, 7 % 7 = 0
+            idx += 1
+    else:
+        for col in range(cols - 1, -1, -1):
+            elements[col] = word[idx % len(word)]
+            idx += 1
+    print("".join(elements))
