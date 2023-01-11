64d1a7bab131228d5ae8447922bca7b0ea49dacd
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-11 22:52:23 +0200

    Update *10.Fill_the_box.py

--- 4.Functions_advanced/*10.Fill_the_box.py
+++ 4.Functions_advanced/*10.Fill_the_box.py
@@ -21,3 +21,38 @@ def fill_the_box(height, length, width, *args):
 print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))
 print(fill_the_box(5, 5, 2, 40, 11, 7, 3, 1, 5, "Finish"))
 print(fill_the_box(10, 10, 10, 40, "Finish", 2, 15, 30))
+
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Write a function called fill_the_box that receives a different number of arguments representing:
+    * the height of a box
+    * the length of a box
+    * the width of a box
+    * n-times a different number of cubes with exact size 1 x 1 x 1
+    * a string "Finish"
+Your task is to fill the box with the given cubes until the current argument equals "Finish".
+The function should return a string in the following format:
+If, at the end, there is free space left in the box, print:
+"There is free space in the box. You could put {free space in cubes} more cubes."
+If there is no free space in the box, print:
+"No more free space! You have {cubes left} more cubes."
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+print(fill_the_box(2, 8, 2, 2, 1, 7, 3, 1, 5, "Finish"))	
+Output
+There is free space in the box. You could put 13 more cube.
+-------------------------------------------------------------
+Test Code	
+print(fill_the_box(5, 5, 2, 40, 11, 7, 3, 1, 5, "Finish"))	
+Output
+No more free space! You have 17 more cubes.
+--------------------------------------------------------------
+Test Code	
+print(fill_the_box(10, 10, 10, 40, "Finish", 2, 15, 30))	
+Output
+There is free space in the box. You could put 960 more cubes.	
+
+"""
