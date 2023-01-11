63282a2773ca4d0d2e2d9eb443a9b3c76b790b98
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-11 22:54:08 +0200

    Update *6.Function_executor.py

--- 4.Functions_advanced/*6.Function_executor.py
+++ 4.Functions_advanced/*6.Function_executor.py
@@ -18,3 +18,47 @@ print(func_executor(
     (sum_numbers, (1, 2)),
     (multiply_numbers, (2, 4))
 ))
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a function called func_executor() that can receive a different number of tuples, each of which will 
+have exactly 2 elements: the first will be a function, and the second will be a tuple of the arguments that
+need to be passed to that function. You should execute each function and return its result in the format:
+        "{function name} - {function result}"
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+print(func_executor(
+    (sum_numbers, (1, 2)), 
+    (multiply_numbers, (2, 4))
+))	
+Output
+sum_numbers - 3
+multiply_numbers - 8
+---------------------------------------------
+Test Code
+def make_upper(*strings):
+    result = tuple(s.upper() for s in strings)
+    return result
+
+def make_lower(*strings):
+    result = tuple(s.lower() for s in strings)
+    return result
+
+print(func_executor(
+    (make_upper, ("Python", "softUni")),
+    (make_lower, ("PyThOn",)),
+))	
+Output
+make_upper - ('PYTHON', 'SOFTUNI')
+make_lower - ('python', )
+
+
+"""
