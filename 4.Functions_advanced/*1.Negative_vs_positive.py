f262b0a03e804920c7e375f1c29b777cf7dfb102
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-11 22:55:41 +0200

    Update *1.Negative_vs_positive.py

--- 4.Functions_advanced/*1.Negative_vs_positive.py
+++ 4.Functions_advanced/*1.Negative_vs_positive.py
@@ -19,3 +19,34 @@ if abs(negative_sum) > positive_sum:
     print("The negatives are stronger than the positives")
 else:
     print("The positives are stronger than the negatives")
+
+    
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+You will receive a sequence of numbers (integers) separated by a single space. Separate the negative numbers
+from the positive. Find the total sum of the negatives and positives, and print the following:
+On the first line, print the sum of the negatives
+On the second line, print the sum of the positives
+On the third line:
+If the absolute negative number is larger than the positive number: 
+    "The negatives are stronger than the positives"
+If the positive number is larger than the absolute negative number:
+    "The positives are stronger than the negatives"
+Note: you will not receive any zeroes in the input.
+-------------------------------------- Example inputs ----------------------------------
+Input
+1 2 -3 -4 65 -98 12 57 -84	
+Output
+-189
+137
+The negatives are stronger than the positives
+----------------------------------------------
+Input
+1 2 3	
+Output
+0
+6
+The positives are stronger than the negatives
+
+"""
