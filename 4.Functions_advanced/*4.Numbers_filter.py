168a88f19ec0de4960c209843da631457a6c59cc
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-11 22:54:46 +0200

    Update *4.Numbers_filter.py

--- 4.Functions_advanced/*4.Numbers_filter.py
+++ 4.Functions_advanced/*4.Numbers_filter.py
@@ -12,3 +12,31 @@ def even_odd_filter(**kwargs):
 
 print(even_odd_filter(odd=[1, 2, 3, 4, 10, 5], even=[3, 4, 5, 7, 10, 2, 5, 5, 2],))
 print(even_odd_filter(odd=[2, 2, 30, 44, 10, 5],))
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a function called even_odd_filter() that can receive a different number of named arguments. The keys 
+will be "even" and/or "odd", and the values will be a list of numbers.  
+When the key is "odd", you should extract only the odd numbers from its list. When the key is "even", you 
+should extract only the even numbers from its list.
+The function should return a dictionary sorted by the length of the lists in descending order. There will 
+be no case of lists with the same length.
+-------------------------------------- Example inputs ----------------------------------
+Input	
+print(even_odd_filter(
+    odd=[1, 2, 3, 4, 10, 5],
+    even=[3, 4, 5, 7, 10, 2, 5, 5, 2],
+))	
+Output
+{'even': [4, 10, 2, 2], 'odd': [1, 3, 5]}
+-------------------------------------------
+Input	
+print(even_odd_filter(
+    odd=[2, 2, 30, 44, 10, 5],
+))	
+Output
+{'odd': [5]}
+
+"""
