d15725a3d7888c017eec4707b8b06d5b53a5abfb
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-11 22:55:21 +0200

    Update *2.Keyword_arguments.py

--- 4.Functions_advanced/*2.Keyword_arguments.py
+++ 4.Functions_advanced/*2.Keyword_arguments.py
@@ -4,3 +4,20 @@ def kwargs_length(**kwargs):
 
 dictionary = {'name': 'Peter', 'age': 25}
 print(kwargs_length(**dictionary))
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a function called kwargs_length() that can receive some keyword arguments and return their length.
+Submit only your function in the judge system.
+-------------------------------------- Example inputs ----------------------------------
+Test Code	Output
+dictionary = {'name': 'Peter', 'age': 25}
+
+print(kwargs_length(**dictionary))	2
+dictionary = {}
+
+print(kwargs_length(**dictionary))	0
+
+"""
