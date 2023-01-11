840d4cb778851b7f8e6044c5d59f548516720cb1
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-11 22:54:23 +0200

    Update *5.Concatenate.py

--- 4.Functions_advanced/*5.Concatenate.py
+++ 4.Functions_advanced/*5.Concatenate.py
@@ -9,3 +9,25 @@ def concatenate(*args, **kwargs):
 
 print(concatenate("Soft", "UNI", " ", "Is", " ", "Grate", "!", UNI="Uni", Grate="Great"))
 print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Write a concatenate() function that receives some strings as arguments and some named arguments (the key 
+will be a string, and the value will be another string).
+First, you should concatenate all arguments successively. Next, take each key successively, and if it is 
+present in the resulted string, change all matching parts with the key's value. In the end, return the 
+final string.
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+print(concatenate("Soft", "UNI", "Is", "Grate", "!", UNI="Uni", Grate="Great"))
+Output
+SoftUniIsGreat!
+---------------------------------------------------------------------------------
+Test Code
+print(concatenate("I", " ", "Love", " ", "Cythons", C="P", s="", java='Java'))	
+Output
+I Love Python
+
+"""
