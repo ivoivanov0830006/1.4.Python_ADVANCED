4a86a8696256b221177d68a83fb96f539b4e8e09
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-11 22:51:56 +0200

    Update *9.Recursion_palindrome.py

--- 4.Functions_advanced/*9.Recursion_palindrome.py
+++ 4.Functions_advanced/*9.Recursion_palindrome.py
@@ -28,5 +28,23 @@ for idx in range(len(word) // 2):
 
 else:
     print(f"{word} is palindrome")
-    
+
+
+------------------------------------ Problem to resolve --------------------------------
+
+Write a recursive function called palindrome() that will receive a word and an index (always 0). 
+Implement the function, so it returns "{word} is a palindrome" if the word is a palindrome and "{word} 
+is not a palindrome" if the word is not a palindrome using recursion. 
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+print(palindrome("abcba", 0))	
+Output
+abcba is a palindrome
+---------------------------------
+Test Code	
+print(palindrome("peter", 0))
+Output
+peter is not a palindrome
+
+"""
     
