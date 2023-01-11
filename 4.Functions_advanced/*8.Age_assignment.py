1be70d7dd4e0fbf152376489782372ccd3a6c53c
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-11 22:53:31 +0200

    Update *8.Age_assignment.py

--- 4.Functions_advanced/*8.Age_assignment.py
+++ 4.Functions_advanced/*8.Age_assignment.py
@@ -12,3 +12,28 @@ def age_assignment(*args, **kwargs):
 
 print(age_assignment("Peter", "George", G=26, P=19))
 print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a function called age_assignment() that receives a different number of names and a different number 
+of key-value pairs. The key will be a single letter (the first letter of each name) and the value - a number
+(age). Find its first letter in the key-value pairs for each name and assign the age to the person's name.
+Then, sort the names in ascending order (alphabetically) and return the information for each person on a 
+new line in the format: 
+    "{name} is {age} years old."
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+print(age_assignment("Peter", "George", G=26, P=19))	
+Output
+George is 26 years old.
+Peter is 19 years old.
+-------------------------------------------------------
+Test Code	
+print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))	Amy is 22 years old.
+Output
+Bill is 61 years old.
+Willy is 36 years old.
+
+"""
