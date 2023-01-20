89ab53c9beae0c31fcae25dc9874368857d7d44d
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-20 22:09:46 +0200

    Update 2.Darts.py

--- 0.Exams_Advanced/2.Darts.py
+++ 0.Exams_Advanced/2.Darts.py
@@ -61,3 +61,41 @@ if p1_wins:
     print(f"{p1} won the game with {total_throws[p1]} throws!")
 if p2_wins:
     print(f"{p2} won the game with {total_throws[p2]} throws!")
+
+    
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+You will be given a matrix with 7 rows and 7 columns representing the dartboard. For example:
+
+1	2	3	4	5	6	7
+24	D	D	D	D	D	8
+23	D	T	T	T	D	9
+22	D	T	B	T	D	10
+21	D	T	T	T	D	11
+20	D	D	D	D	D	12
+19	18	17	16	15	14	13
+
+Each of the two players starts with a score of 501 and they take turns to throw a dart – one throw
+for each player. The score for each turn is deducted from the player’s total score. The first 
+player who reduces their score to zero or less wins the game.
+You are going to receive the information for every throw on a separate line. The coordinate 
+information of a hit will be in the format: 
+        "({row}, {column})".
+    * If a player hits outside the dartboard, he does not score any points.
+    * If a player hits a number, it is deducted from his total.
+    * If a player hits a "D" the sum of the 4 corresponding numbers per column and row is doubled 
+    and then deducted from his total.
+    * If a player hits a "T" the sum of the 4 corresponding numbers per column and row is tripled 
+    and then deducted from his total.
+"B" is the bullseye. 
+    * If a player hits it, he wins the game, and the program ends.
+For example, if Peter hits position with coordinates (2, 1), he wins (23 + 2 + 9 + 18) * 2 = 104 points 
+and they are deducted from his total. Your job is to find who won the game and with how many turns.
+You should print only one line containing the winner and his count of throws: 
+        "{name} won the game with {count_turns} throws!"
+Constrains
+There will always be exactly 7 lines
+There will always be a winner
+The points will be in range [1, 24]
+The coordinates will be in range [0, 100]    
