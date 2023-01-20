bbce1a7dd7330af1a16e03ab4a3dd833573e04d2
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-20 21:50:04 +0200

    Update 2.Ball_in_bucket.py

--- 0.Exams_Advanced/2.Ball_in_bucket.py
+++ 0.Exams_Advanced/2.Ball_in_bucket.py
@@ -70,3 +70,31 @@ Constraints
 All of the given points will be integers in the range [1, 30]
 All the given indexes will be integers in the range [0, 30]
 There always will be exactly 6 buckets - 1 on each column
+-------------------------------------- Example inputs ----------------------------------
+Input	
+10 30 B 4 20 24
+7 8 27 23 11 19
+13 3 14 B 17 В
+12 5 21 22 9 6
+B 26 1 28 29 2
+25 B 16 15 B 18
+(1, 1)
+(20, 15)
+(4, 0)	
+Output
+Sorry! You need 33 points more to win a prize.
+-------------------------------------------------
+Input	
+B 30 14 23 20 24
+29 8 27 18 11 19
+13 3 B B 17 6
+28 5 21 22 9 B
+10 B 26 12 B 2
+25 1 16 15 7 4
+(0, 0)
+(2, 2)
+(2, 3)	
+Output
+Good job! You scored 299 points, and you've won Teddy Bear.
+
+"""
