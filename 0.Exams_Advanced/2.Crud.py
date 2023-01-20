7275c1b7c4aeb9cac72d3451a94a2b1dfdafdc97
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-20 22:01:09 +0200

    Update 2.Crud.py

--- 0.Exams_Advanced/2.Crud.py
+++ 0.Exams_Advanced/2.Crud.py
@@ -52,3 +52,44 @@ while True:
 
 for row in matrix:
     print(*row)
+
+    
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+In the beginning, you will be given a matrix with 6 rows and 6 columns - a table with information. 
+It consists of:
+    Letters - on one or many positions in the table
+    Numbers - on one or many positions in the table
+    Empty positions - marked with "."
+Next, you will receive your first position on the table in the format:
+            "({row}, {column})"
+On the following lines, until you receive "Stop" you will be receiving commands in the format:
+            "Create, {direction}, {value}"
+The direction could be "up", "down", "left" or "right"
+If you step in an empty position, create the given value on that position. E.g., 
+if the given value is "A", and the position is empty (".") - change it to "A"
+If the position is NOT empty, do NOT create a value on that position
+            "Update, {direction}, {value}"
+The direction could be "up", "down", "left" or "right"
+If you step on a letter or number, update the position with the given value. E.g., 
+if the given value is "h", and the position's value is "12" - change it to "h"
+If the position is empty, do NOT update the value on that position
+            "Delete, {direction}"
+The direction could be "up", "down", "left" or "right"
+If you step on a letter or number, delete it, and empty the position. E.g., 
+if the given position's value is "h" - change it to "."
+If the position is already empty, do NOT delete it
+            "Read, {direction}"
+The direction could be "up", "down", "left" or "right"
+If you step on a letter or number, print it on the console
+If the position is empty, do NOT read it
+You can make only ONE move at a time in the given direction for each command given.
+In the end, print the final matrix.
+
+Output
+In the end, print the final matrix, each row on a new line, each position separated by a single space.
+Constraints
+You will always receive valid coordinates
+You will always receive directions in the range of the table
+You will always receive letters or numbers
