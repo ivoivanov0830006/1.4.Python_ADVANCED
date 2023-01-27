209be3e7ec71e91a99b596090fcaf222c0667034
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-27 23:18:51 +0200

    Create 3.Spring_time.py

---
+++ 0.Exams_Advanced/3.Spring_time.py
@@ -0,0 +1,17 @@
+def start_spring(**kwargs):
+    result = ""
+    spring_objects = {}
+    for value, key in kwargs.items():
+        if key not in spring_objects:
+            spring_objects[key] = []
+        spring_objects[key].append(value)
+
+    sorted_spring_objects = sorted(spring_objects.items(), key=lambda x: (-len(x[1]), x[0]))
+    for item in spring_objects.items():
+        type_object = item[0]
+        name_objects = item[1]
+        result += f"{type_object}\n"
+        for obj in name_objects:
+            result += f"-{obj}\n"
+    return result
+
