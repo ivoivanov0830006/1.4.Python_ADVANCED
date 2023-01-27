65cf07f61987f1a38d2e23a7cd4a20e43b6c3976
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-27 22:30:27 +0200

    Update 3.Words_sorting.py

--- 0.Exams_Advanced/3.Words_sorting.py
+++ 0.Exams_Advanced/3.Words_sorting.py
@@ -27,3 +27,19 @@ print(
         'eye'
   ))
 
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Write a function words_sorting which receives a different number of words.
+Create a dictionary, which will have as keys the words that the function received. For each key, create a 
+value that is the sum of all ASCII values of that key. Then, sort the dictionary:
+    * By values in descending order, if the sum of all values of the dictionary is odd
+    * By keys in ascending order, if the sum of all values of the dictionary is even
+Input
+There will be no input, just any number of words passed to your function
+Output
+The function should return a string in the format "{key} - {value}" for each key and value on a separate lines
+Constraints:
+There will be no case with capital letters.
+There will be no case with a string consisting of other than letters.
