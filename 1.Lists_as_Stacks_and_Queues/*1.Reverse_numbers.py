407080f17faccca11b9580619c01bf9a127d8633
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-28 21:23:45 +0200

    Update *1.Reverse_numbers.py

--- 1.Lists_as_Stacks_and_Queues/*1.Reverse_numbers.py
+++ 1.Lists_as_Stacks_and_Queues/*1.Reverse_numbers.py
@@ -1,17 +1,32 @@
+from collections import deque
+
+numbers = deque(input().split())
+numbers.reverse()
+
+print(" ".join(numbers))
+
+
+"""
+------------------------------------- Another Solution -----------------------------
 numbers = input().split()
 stack = []
-print(numbers)
+
 for index in range(len(numbers)):
     stack.append(numbers.pop())
 
 print(" ".join(stack))
 
 
-# ------------------------------------- Problem to resolve ------------------------------
-#
-# Write a program that reads a string with N integers from the console, separated by a single space,
-# and reverses them using a stack. Print the reversed integers on one line, separated by a single space.
-# -------------------------------------- Example inputs ----------------------------------
-# Input	                Output
-# 1 2 3 4 5	            5 4 3 2 1
-# 1	                    1
+------------------------------------- Another Solution -----------------------------
+print(*input().split()[::-1], sep=" ")
+
+
+------------------------------------- Problem to resolve ------------------------------
+
+Write a program that reads a string with N integers from the console, separated by a single space,
+and reverses them using a stack. Print the reversed integers on one line, separated by a single space.
+-------------------------------------- Example inputs ----------------------------------
+Input	                Output
+1 2 3 4 5	            5 4 3 2 1
+1	                    1
+"""
