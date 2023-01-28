433ce5fb6757d7ecda164c7491a8188902ec3051
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-28 21:39:03 +0200

    Update *1.Reverse_numbers.py

--- 1.Lists_as_Stacks_and_Queues/*1.Reverse_numbers.py
+++ 1.Lists_as_Stacks_and_Queues/*1.Reverse_numbers.py
@@ -26,7 +26,11 @@ print(*input().split()[::-1], sep=" ")
 Write a program that reads a string with N integers from the console, separated by a single space,
 and reverses them using a stack. Print the reversed integers on one line, separated by a single space.
 -------------------------------------- Example inputs ----------------------------------
-Input	                Output
-1 2 3 4 5	            5 4 3 2 1
-1	                    1
+Input	                
+1 2 3 4 5	            
+1
+Output
+5 4 3 2 1
+1
+
 """
