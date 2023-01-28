8124fc71e1cac84f39677cd168f4066f99f11ffe
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-28 21:42:40 +0200

    Update *4.Fashion_boutique.py

--- 1.Lists_as_Stacks_and_Queues/*4.Fashion_boutique.py
+++ 1.Lists_as_Stacks_and_Queues/*4.Fashion_boutique.py
@@ -1,28 +1,24 @@
-sequence = input().split()
-clothes = []
-
+clothes = list(map(int, input().split()))
 rack_capacity = int(input())
-
-for i in range(len(sequence)):
-    clothes.append(sequence[i])
 
 rack_count = 1
 current_rack = 0
 
-while len(clothes) > 0:
-    current_clothes = int(clothes.pop())
-    current_rack += current_clothes
-    if current_rack == rack_capacity:
-        current_rack = 0
+for _ in range(len(clothes)):
+    if current_rack + clothes[-1] > rack_capacity:
         rack_count += 1
-    elif current_rack > rack_capacity:
-        clothes.append(current_clothes)
+        current_rack = clothes.pop()
+    elif current_rack + clothes[-1] == rack_capacity:
         current_rack = 0
-        rack_count += 1
-        current_rack += current_clothes
         clothes.pop()
+        if clothes:
+            rack_count += 1
+
+    else:
+        current_rack += clothes.pop()
 
 print(rack_count)
+
 
 
 """
