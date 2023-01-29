f2ac6353a37023fa62542340f0c736b61f9547b0
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-29 16:21:23 +0200

    Update *6.Balanced_parentheses.py

--- 1.Lists_as_Stacks_and_Queues/*6.Balanced_parentheses.py
+++ 1.Lists_as_Stacks_and_Queues/*6.Balanced_parentheses.py
@@ -21,3 +21,27 @@ if not balanced or opening_brackets:
     print("NO")
 else:
     print("YES")
+
+    
+"""
+------------------------------------- Another Solution -----------------------------
+
+from collections import deque
+
+parentheses = deque(input())
+open_parentheses = deque()
+
+while parentheses:
+    left_parenthesis = parentheses.popleft()
+
+    if left_parenthesis in "{([":
+        open_parentheses.append(left_parenthesis)
+    elif not open_parentheses:
+        print("NO")
+        break
+    else:
+        if f"{open_parentheses.pop() + left_parenthesis}" not in "{}()[]":
+            print("NO")
+            break
+else:
+    print("YES")
