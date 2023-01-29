a73cb50571a75c5d7a8239d879b36bd307251569
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-29 16:38:04 +0200

    Update 5.Hot_potato.py

--- 1.Lists_as_Stacks_and_Queues/5.Hot_potato.py
+++ 1.Lists_as_Stacks_and_Queues/5.Hot_potato.py
@@ -18,46 +18,18 @@ last_child = children.popleft()
 print(f"Last is {last_child}")
 
 
-# ------------------------------------- Another Solution -----------------------------
-#
-# from collections import deque
-#
-# children = deque(input().split())
-# kicked_child = int(input())
-#
-# while len(children) > 1:
-#     children.rotate(-kicked_child)
-#     child = children.pop()
-#     print(f"Removed {child}")
-#
-# last_child = children.popleft()
-# print(f"Last is {last_child}")
+"""
+------------------------------------- Another Solution -----------------------------
 
+from collections import deque
 
-# ------------------------------------- Problem to resolve ------------------------------
-#
-# Hot Potato is a game in which children form a circle and toss a hot potato. The counting starts with
-# the first kid. Every nth toss, the child holding the potato leaves the game. When a kid leaves the
-# game, it passes the potato to the next kid. It continues until there is only one kid left.
-# Create a program that simulates the game of Hot Potato. On the first line, you will receive kids'
-# names, separated by a single space. On the second line, you will receive the nth toss (integer) in
-# which a child leaves the game.
-# Print every kid who is removed from the circle in the format "Removed {kid}". In the end, print the
-# only kid left in the format "Last is {kid}".
-# -------------------------------------- Example inputs ----------------------------------
-# Input	                                    Output
-# Tracy Emily Daniel                        Removed Emily
-# 2	                                        Removed Tracy
-#                                           Last is Daniel
-# --------------------------------------------------------
-# George Peter Michael William Thomas       Removed Thomas
-# 10                                        Removed Peter
-#                                           Removed Michael
-#                                           Removed George
-#                                           Last is William
-# ---------------------------------------------------------
-# George Peter Michael William Thomas       Removed George
-# 1	                                        Removed Peter
-#                                           Removed Michael
-#                                           Removed William
-#                                           Last is Thomas
+children = deque(input().split())
+kicked_child = int(input())
+
+while len(children) > 1:
+    children.rotate(-kicked_child)
+    child = children.pop()
+    print(f"Removed {child}")
+
+last_child = children.popleft()
+print(f"Last is {last_child}")
