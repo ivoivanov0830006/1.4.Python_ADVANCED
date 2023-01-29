c0e86c95a4b972ab7a0646eba8cb7175625c8bbc
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-29 16:32:10 +0200

    Update 3.Supermarket.py

--- 1.Lists_as_Stacks_and_Queues/3.Supermarket.py
+++ 1.Lists_as_Stacks_and_Queues/3.Supermarket.py
@@ -15,27 +15,38 @@ while True:
 print(f"{len(people)} people remaining.")
 
 
-# ------------------------------------- Problem to resolve ------------------------------
-#
-# Tom is working at the supermarket, and he needs your help to keep track of his clients.
-# Write a program that reads lines of input consisting of a customer's name and adds it to the end of a
-# queue until "End" is received. If, in the meantime, you receive the command "Paid", you should print
-# each customer in the order they are served (from the first to the last one) and empty the queue.
-# When you receive "End", you should print the count of the remaining people in the queue in the format:
-#   "{count} people remaining.".
-# -------------------------------------- Example inputs ----------------------------------
-# Input	                        Output
-# George                        George
-# Peter                         Peter
-# William                       William
-# Paid                          4 people remaining.
-# Michael
-# Oscar
-# Olivia
-# Linda
-# End
-# ---------------------------------------------------
-# Anna                          3 people remaining.
-# Emma
-# Alexander
-# End
+"""
+------------------------------------- Problem to resolve ------------------------------
+
+Tom is working at the supermarket, and he needs your help to keep track of his clients.
+Write a program that reads lines of input consisting of a customer's name and adds it to the end of a
+queue until "End" is received. If, in the meantime, you receive the command "Paid", you should print
+each customer in the order they are served (from the first to the last one) and empty the queue.
+When you receive "End", you should print the count of the remaining people in the queue in the format:
+  "{count} people remaining.".
+-------------------------------------- Example inputs ----------------------------------
+Input	                        
+George                        
+Peter                         
+William                       
+Paid                          
+Michael
+Oscar
+Olivia
+Linda
+End
+Output
+George
+Peter
+William
+4 people remaining.
+---------------------------------------------------
+Input
+Anna                          
+Emma
+Alexander
+End
+Output
+3 people remaining.
+
+"""
