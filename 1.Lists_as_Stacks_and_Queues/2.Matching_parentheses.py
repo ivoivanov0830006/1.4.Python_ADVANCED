0e6bd37fc3d4f2ebe5b3080c875e850f2f06fe54
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-29 16:30:13 +0200

    Update 2.Matching_parentheses.py

--- 1.Lists_as_Stacks_and_Queues/2.Matching_parentheses.py
+++ 1.Lists_as_Stacks_and_Queues/2.Matching_parentheses.py
@@ -11,19 +11,28 @@ for index in range(len(text)):
         print(parentheses)
 
 
-# ------------------------------------- Problem to resolve ------------------------------
-#
-# You are given an algebraic expression with parentheses.
-# Scan through the string and extract each set of parentheses.
-#   * If you find an opening parenthesis, push the index into the stack.
-#   * If you find a closing parenthesis, pop the topmost element from the stack. This is the index
-#   of the last opening parenthesis.
-#   * Use the current index and the popped one to extract a set of parentheses.
-# Print the result back on the console.
-# -------------------------------------- Example inputs ----------------------------------
-# Input	Output
-# 1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5	(2 + 3)
-# (3 + 1)
-# (2 - (2 + 3) * 4 / (3 + 1))
-# (2 + 3) - (2 + 3)	(2 + 3)
-# (2 + 3)
+"""
+------------------------------------- Problem to resolve ------------------------------
+
+You are given an algebraic expression with parentheses.
+Scan through the string and extract each set of parentheses.
+  * If you find an opening parenthesis, push the index into the stack.
+  * If you find a closing parenthesis, pop the topmost element from the stack. This is the index
+  of the last opening parenthesis.
+  * Use the current index and the popped one to extract a set of parentheses.
+Print the result back on the console.
+-------------------------------------- Example inputs ----------------------------------
+Input	                                
+1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+Output
+(2 + 3)
+(3 + 1)                               
+(2 - (2 + 3) * 4 / (3 + 1))                                 
+------------------------------------------------------------------
+Input
+(2 + 3) - (2 + 3)
+Output
+(2 + 3)
+(2 + 3)
+
+"""
