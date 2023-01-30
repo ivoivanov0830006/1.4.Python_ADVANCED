239804a468899ad9b457fe889171bdf1f0c2d5e0
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-30 23:27:18 +0200

    Update 4.Parking_lot.py

--- 2.Tuples_and_Sets/4.Parking_lot.py
+++ 2.Tuples_and_Sets/4.Parking_lot.py
@@ -14,34 +14,45 @@ else:
     print("Parking Lot is Empty")
 
 
-# ------------------------------------- Problem to resolve ------------------------------
-#
-# Write a program that:
-#   * Records a car number for every car that enters the parking lot
-#   * Removes a car number when the car leaves the parking lot
-# On the first line, you will receive the number of commands - N. On the following N lines, you will
-# receive the direction and car's number in the format:
-#       "{direction}, {car_number}". The direction could
-# only be "IN" or "OUT". Print the car numbers which are still in the parking lot. Keep in mind that all
-# car numbers must be unique. If the parking lot is empty, print:
-#       "Parking Lot is Empty"
-# Note: The order in which we print the result does not matter.
-# -------------------------------------- Example inputs ----------------------------------
-# Input	                            Output
-# 10                                CA2844AA
-# IN, CA2844AA                      CA9999TT
-# IN, CA1234TA                      CA2822UU
-# OUT, CA2844AA                     CA9876HH
-# IN, CA9999TT
-# IN, CA2866HI
-# OUT, CA1234TA
-# IN, CA2844AA
-# OUT, CA2866HI
-# IN, CA9876HH
-# IN, CA2822UU
-# --------------------------------------------------------------
-# 4                                 Parking Lot is Empty
-# IN, CA2844AA
-# IN, CA1234TA
-# OUT, CA2844AA
-# OUT, CA1234TA
+"""
+------------------------------------- Problem to resolve ------------------------------
+
+Write a program that:
+  * Records a car number for every car that enters the parking lot
+  * Removes a car number when the car leaves the parking lot
+On the first line, you will receive the number of commands - N. On the following N lines, you will
+receive the direction and car's number in the format:
+      "{direction}, {car_number}". The direction could
+only be "IN" or "OUT". Print the car numbers which are still in the parking lot. Keep in mind that all
+car numbers must be unique. If the parking lot is empty, print:
+      "Parking Lot is Empty"
+Note: The order in which we print the result does not matter.
+-------------------------------------- Example inputs ----------------------------------
+Input	                            
+10                                
+IN, CA2844AA                      
+IN, CA1234TA                      
+OUT, CA2844AA                     
+IN, CA9999TT
+IN, CA2866HI
+OUT, CA1234TA
+IN, CA2844AA
+OUT, CA2866HI
+IN, CA9876HH
+IN, CA2822UU
+Output
+CA2844AA
+CA9999TT
+CA2822UU
+CA9876HH
+--------------------------------------------------------------
+Input
+4                                 
+IN, CA2844AA
+IN, CA1234TA
+OUT, CA2844AA
+OUT, CA1234TA
+Output
+Parking Lot is Empty
+
+"""
