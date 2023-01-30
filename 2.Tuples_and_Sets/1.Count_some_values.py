5522366036d8bf6bafe4be0f3f66de2bd770a20e
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-30 22:43:10 +0200

    Update 1.Count_some_values.py

--- 2.Tuples_and_Sets/1.Count_some_values.py
+++ 2.Tuples_and_Sets/1.Count_some_values.py
@@ -23,20 +23,36 @@ for element in sequence:
         all_elements.append(element)
         print(f"{number:.1f} - {count} times")
 
-# ------------------------------------- Problem to resolve ------------------------------
-#
-# You will be given numbers separated by a space. Write a program that prints the number of occurrences
-# of each number in the format "{number} - {count} times". The number must be formatted to the first
-# decimal point.
-# -------------------------------------- Example inputs ----------------------------------
-# Input	                                        Output
-# -2.5 4 3 -2.5 -5.5 4 3 3 -2.5 3               -2.5 - 3 times
-# 	                                            4.0 - 2 times
-#                                               3.0 - 4 times
-#                                               -5.5 - 1 times
-# ------------------------------------------------------
-# 2 4 4 5 5 2 3 3 4 4 3 3 4 3 5 3 2 5 4 3       2.0 - 3 times
-# 	                                            4.0 - 6 times
-#                                               5.0 - 4 times
-#                                               3.0 - 7 times
+        
+------------------------------------- Another Solution -----------------------------
 
+sequence = tuple(map(float, input().split()))
+counter_of_values = {value: sequence.count(value) for value in sequence}
+
+for number, count in counter_of_values.items():
+    print(f"{number:.1f} - {count} times")
+    
+    
+------------------------------------- Problem to resolve ------------------------------
+
+You will be given numbers separated by a space. Write a program that prints the number of occurrences
+of each number in the format "{number} - {count} times". The number must be formatted to the first
+decimal point.
+-------------------------------------- Example inputs ----------------------------------
+Input	                                        
+-2.5 4 3 -2.5 -5.5 4 3 3 -2.5 3               
+Output	                                            
+-2.5 - 3 times
+4.0 - 2 times                                    
+3.0 - 4 times
+-5.5 - 1 times
+------------------------------------------------------
+Input
+2 4 4 5 5 2 3 3 4 4 3 3 4 3 5 3 2 5 4 3       
+Output	 
+2.0 - 3 times
+4.0 - 6 times
+5.0 - 4 times
+3.0 - 7 times
+
+"""
