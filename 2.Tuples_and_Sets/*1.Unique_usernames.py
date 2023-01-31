1aa0e6d6f9db9aecf3088a0923598cad8f23f912
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-01-31 23:54:51 +0200

    Update *1.Unique_usernames.py

--- 2.Tuples_and_Sets/*1.Unique_usernames.py
+++ 2.Tuples_and_Sets/*1.Unique_usernames.py
@@ -8,29 +8,49 @@ for _ in range(number_usernames):
 print("\n".join(unique_usernames))
 
 
-# ------------------------------------- Problem to resolve ------------------------------
-#
-# Write a program that reads from the console a sequence of N usernames and keeps a collection only of the
-# unique ones. On the first line, you will receive an integer N. On the next N lines, you will receive a
-# username. Print the collection on the console (the order does not matter):
-# -------------------------------------- Example inputs ----------------------------------
-# Input	            Output
-# 6                 George
-# George            Peter
-# George            NiceGuy1234
-# George
-# Peter
-# George
-# NiceGuy1234
-# ----------------------------------
-# 10                Peter
-# Peter             Maria
-# Maria             George
-# Peter             Steve
-# George            Alex
-# Steve
-# Maria
-# Alex
-# Peter
-# Steve
-# George
+"""
+------------------------------------- Another Solution -----------------------------
+
+print(*{input() for _ in range(int(input()))}, sep="\n")
+
+
+------------------------------------- Problem to resolve ------------------------------
+
+Write a program that reads from the console a sequence of N usernames and keeps a collection only of the
+unique ones. On the first line, you will receive an integer N. On the next N lines, you will receive a
+username. Print the collection on the console (the order does not matter):
+-------------------------------------- Example inputs ----------------------------------
+Input	            
+6                 
+George            
+George            
+George
+Peter
+George
+NiceGuy1234
+Output
+George
+Peter
+NiceGuy1234
+----------------------------------
+Input
+10                
+Peter             
+Maria             
+Peter             
+George            
+Steve
+Maria
+Alex
+Peter
+Steve
+George
+Output
+Peter
+Maria
+George
+Steve
+Alex
+
+"""
+
