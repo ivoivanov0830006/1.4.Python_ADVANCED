b2714696d576457b5470e2daac99abf36b977a9e
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-01 17:14:28 +0200

    Update *5.Longest_intersection.py

--- 2.Tuples_and_Sets/*5.Longest_intersection.py
+++ 2.Tuples_and_Sets/*5.Longest_intersection.py
@@ -28,26 +28,22 @@ longest_range = (', '.join(str(x) for x in longest_range))
 print(f"Longest intersection is [{longest_range}] with length {longest_length}")
 
 
-# ------------------------------------- Problem to resolve ------------------------------
-#
-# Write a program that finds the longest intersection. You will be given a number N. On each of the next
-# N lines you will be given two ranges in the format:
-#       "{first_start},{first_end}-{second_start},{second_end}".
-# You should find the intersection of these two ranges. The start and end numbers in the ranges are inclusive.
-# Finally, you should find the longest intersection of all N intersections, print the numbers that are included
-# and its length in the format:
-#       "Longest intersection is [{longest_intersection_numbers}] with length {length_longest_intersection}"
-# Note: in each range, there will always be an intersection. If there are two equal intersections, print the first one.
-# -------------------------------------- Example inputs ----------------------------------
-# Input	                    Output
-# 3                         Longest intersection is [6, 7, 8, 9, 10] with length 5
-# 0,3-1,2
-# 2,10-3,5
-# 6,15-3,10
-# -----------------------------------------------------------------------------------------
-# 5                         Longest intersection is [2, 3, 4, 5, 6, 7, 8, 9, 10] with length 9
-# 0,10-2,5
-# 3,8-1,7
-# 1,8-2,4
-# 4,7-2,5
-# 1,10-2,11
+"""
+------------------------------------- Another Solution -----------------------------
+
+longest_intersection = set()
+
+for _ in range(int(input())):
+    first_data, second_data = [el.split(",") for el in input().split("-")]
+
+    first_range = set(range(int(first_data[0]), int(first_data[1]) + 1))
+    second_range = set(range(int(second_data[0]), int(second_data[1]) + 1))
+
+    intersection = first_range.intersection(second_range)
+
+    if len(longest_intersection) < len(intersection):
+        longest_intersection = intersection
+
+longest_range = (', '.join(str(x) for x in longest_intersection))
+print(f"Longest intersection is [{longest_range}] with length {len(longest_intersection)}")
+
