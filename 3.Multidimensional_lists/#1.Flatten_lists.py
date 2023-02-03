014ade7191f85aa622e3c7d07594cf7fb62e84d7
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-03 17:01:33 +0200

    Update #1.Flatten_lists.py

--- 3.Multidimensional_lists/#1.Flatten_lists.py
+++ 3.Multidimensional_lists/#1.Flatten_lists.py
@@ -20,3 +20,18 @@ for sub_string in input_sequence[::-1]:
 
 print(*sub_lists)
 
+
+------------------------------------ Problem to resolve --------------------------------
+
+Write a program to flatten several lists of numbers received in the following format:
+⦁	String with numbers or empty strings separated by "|"
+⦁	Values are separated by spaces (" ", one or several)
+⦁	Order the output list from the last to the first matrix sub-lists and their values from left to 
+right as shown below
+Examples
+Input	Output
+1 2 3 |4 5 6 |  7  88	7 88 4 5 6 1 2 3
+7 | 4  5|1 0| 2 5 |3	3 2 5 1 0 4 5 7
+1| 4 5 6 7  |  8 9	8 9 4 5 6 7 1
+
+"""
