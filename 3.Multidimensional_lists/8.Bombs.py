87177abcd3d71a39a46a6f6585684cf34a4765e3
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-03 16:47:28 +0200

    Update 8.Bombs.py

--- 3.Multidimensional_lists/8.Bombs.py
+++ 3.Multidimensional_lists/8.Bombs.py
@@ -93,3 +93,30 @@ print(f"Alive cells: {alive_cells_count}")
 print(f"Sum: {alive_cells_sum}")
 for current_row in matrix:
     print(*current_row, sep=" ")
+    
+    
+------------------------------------ Problem to resolve --------------------------------
+
+You will be given a square matrix of integers, each integer separated by a single space, and each current_row will 
+be on a new line. On the last line of input, you will receive indexes - coordinates of several cells separated
+by a single space, in the following format: "{row1},{column1} {row2},{column2} … {row3},{column3}".
+On those cells, there are bombs. You must detonate every bomb in the order they were given. When a bomb 
+explodes, it deals damage equal to its integer value to all the cells around it (in every direction and in
+all diagonals). One bomb can't explode more than once, and after it does, its value becomes 0. When a cell's
+value reaches 0 or below, it dies. Dead cells can't explode.
+You must print the count of all alive cells and their sum. Afterward, print the matrix with all its cells (including the dead ones).
+Input
+⦁	On the first line, you are given the integer N - the size of the square matrix.
+⦁	The following N lines hold each column's values - N numbers separated by a space.
+⦁	On the last line, you will receive the coordinates of the cells with the bombs in the format described above.
+Output
+⦁	On the first line, you need to print the count of all alive cells in the format: 
+"Alive cells: {alive_cells}"
+⦁	On the second line, you need to print the sum of all alive cells in the format: 
+
+⦁	In the end, print the matrix. A space must separate the cells.
+Constraints
+⦁	The size of the matrix will be between [0…1000].
+⦁	The bomb coordinates will always be in the matrix.
+⦁	The bomb's values will always be greater than 0.
+⦁	The integers of the matrix will be in the range [1…10000]. 
