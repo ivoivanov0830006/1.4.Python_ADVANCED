0493776e34a8d527a4d42ab2efdff946fd67403e
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-07 23:03:46 +0200

    Update *3.Even_odd.py

--- 4.Functions_advanced/*3.Even_odd.py
+++ 4.Functions_advanced/*3.Even_odd.py
@@ -1,13 +1,9 @@
 def even_odd(*args):
-    command = args[-1]
-    checking = 0 if command == "even" else 1
-    result = []
-    for idx in range(len(args) - 1):
-        number = args[idx]
-        if number % 2 == checking:
-            result.append(number)
-
-    return result
+    command = args[-1]  # can't be popped because it's tuple
+    if command == "even":
+        return [n for n in args[:-1] if n % 2 == 0]
+    elif command == "odd":
+        return [n for n in args[:-1] if n % 2 == 1]
 
 
 print(even_odd(1, 2, 3, 4, 5, 6, "even"))
@@ -18,11 +14,15 @@ print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
 ------------------------------------- Another Solution ---------------------------------
 
 def even_odd(*args):
+    command = args[-1]
     result = []
-    if args[-1] == "even":
-        result = [x for x in args[:len(args) - 1] if x % 2 == 0]
-    elif args[-1] == "odd":
-        result = [x for x in args[:len(args) - 1] if x % 2 != 0]
+
+    for n in args[-1]:
+        if int(n) % 2 == 0 and command == "even":
+            result.append(n)
+        elif int(n) % 2 == 1 and command == "odd":
+            result.append(n)
+
     return result
 
 
@@ -32,11 +32,12 @@ print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))
 
 ------------------------------------ Problem to resolve --------------------------------
 
-Create a function called even_odd() that can receive a different quantity of numbers and a command at the 
-end. The command can be "even" or "odd". Filter the numbers depending on the command and return them in a 
+Create a function called even_odd() that can receive a different quantity of numbers and a direction at the 
+end. The direction can be "even" or "odd". Filter the numbers depending on the direction and return them in a 
 list. Submit only the function in the judge system.
 -------------------------------------- Example inputs ----------------------------------
 print(even_odd(1, 2, 3, 4, 5, 6, "even"))	[2, 4, 6]
 print(even_odd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "odd"))	[1, 3, 5, 7, 9]
 
 """
+
