4b6f7c3c710975aa7040cc80160185f6c7b2e39d
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-07 23:04:30 +0200

    Update *4.Numbers_filter.py

--- 4.Functions_advanced/*4.Numbers_filter.py
+++ 4.Functions_advanced/*4.Numbers_filter.py
@@ -13,6 +13,40 @@ print(even_odd_filter(odd=[2, 2, 30, 44, 10, 5],))
 
 
 """
+------------------------------------- Another Solution ---------------------------------
+
+def even_odd_filter(**kwargs):
+    result = {}
+    if "odd" in kwargs:
+        kwargs["odd"] = [n for n in kwargs["odd"] if n % 2 == 1]
+    if "even" in kwargs:
+        kwargs["even"] = [n for n in kwargs["even"] if n % 2 == 0]
+    
+    return {key: value for key, value in sorted(kwargs.items(), key=lambda x: -len(x[1]))}
+        
+        
+print(even_odd_filter(odd=[1, 2, 3, 4, 10, 5], even=[3, 4, 5, 7, 10, 2, 5, 5, 2],))
+print(even_odd_filter(odd=[2, 2, 30, 44, 10, 5],))
+
+
+------------------------------------- Another Solution ---------------------------------
+
+def even_odd_filter(**kwargs):
+    result = {}
+
+    for key, value in kwargs.items():
+        checking = 0 if key == "even" else 1
+        filtered = [x for x in value if x % 2 == checking]
+        result[key] = filtered
+
+    return {key: value for key, value in sorted(result.items(), key=lambda x: -len(x[1]))}
+    # -len(x[1]) is giving descending order
+
+
+print(even_odd_filter(odd=[1, 2, 3, 4, 10, 5], even=[3, 4, 5, 7, 10, 2, 5, 5, 2],))
+print(even_odd_filter(odd=[2, 2, 30, 44, 10, 5],))
+
+
 ------------------------------------ Problem to resolve --------------------------------
 
 Create a function called even_odd_filter() that can receive a different number of named arguments. The keys 
