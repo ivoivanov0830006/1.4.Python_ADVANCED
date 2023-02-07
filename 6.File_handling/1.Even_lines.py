8bfe5000f1b67f1d87d747911de91b38c0a864b0
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-07 22:58:47 +0200

    Create 1.Even_lines.py

---
+++ 6.File_handling/1.Even_lines.py
@@ -0,0 +1,13 @@
+symbols = ["-", ",", ".", "!", "?"]
+
+# even_lines_file = open("files/text.txt", "r")
+
+with open("text.txt", "r") as even_lines_file:
+    text = even_lines_file.readlines()
+
+for row in range(0, len(text), 2):
+
+    for symbol in symbols:
+        text[row] = text[row].replace(symbol, "@")
+
+    print(*text[row].split()[::-1], sep=" ")
