d84295c4e7299223f3103dfe18a9fe482a45cfea
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-07 22:59:48 +0200

    Update 2.Line_numbers.py

--- 6.File_handling/2.Line_numbers.py
+++ 6.File_handling/2.Line_numbers.py
@@ -11,3 +11,19 @@ for row in range(0, len(text), 2):
         text[row] = text[row].replace(symbol, "@")
 
     print(*text[row].split()[::-1], sep=" ")
+
+    
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Write a program that reads a text file and prints on the console its even lines. Line numbers 
+start from 0. Before you print the result, replace {"-", ",", ".", "!", "?"} with "@" and reverse 
+the order of the words.
+-------------------------------------- Example inputs ----------------------------------
+text.txt	output
+-I was quick to judge him, but it wasn't his fault.
+-Is this some kind of joke?! Is it?
+-Quick, hide here. It is safer.	fault@ his wasn't it but him@ judge to quick was @I
+safer@ is It here@ hide @Quick@
+
+"""
