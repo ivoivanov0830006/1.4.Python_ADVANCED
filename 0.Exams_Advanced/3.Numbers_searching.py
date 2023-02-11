404b5ea2ccef93e873d9d304d59b1bf3c091335e
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-11 17:03:53 +0200

    Update 3.Numbers_searching.py

--- 0.Exams_Advanced/3.Numbers_searching.py
+++ 0.Exams_Advanced/3.Numbers_searching.py
@@ -27,3 +27,22 @@ def numbers_searching(*args):
 
 
 print(numbers_searching(50, 50, 47, 47, 48, 45, 49, 44, 47, 45, 44, 44, 48, 44, 48))
+
+
+"""
+------------------------------------- Another Solution ---------------------------------
+
+def numbers_searching(*args):
+    check_for_missing = set(sorted(args))
+    first = min(check_for_missing)
+    last = max(check_for_missing)
+    full_list = [x for x in range(first, last + 1)]
+    missing = list(set(full_list) - check_for_missing)
+
+    duplicates = []
+    find_duplicates = set(args)
+    for x in find_duplicates:
+        if args.count(x) > 1:
+            duplicates.append(x)
+
+    return [*missing, sorted(duplicates)]
