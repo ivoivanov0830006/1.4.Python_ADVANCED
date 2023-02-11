4a24c91652ea01cb8995b8efda28402feafa9a80
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-11 17:03:35 +0200

    Create 3.Numbers_searching.py

---
+++ 0.Exams_Advanced/3.Numbers_searching.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+
+def numbers_searching(*args):
+    max_num = max(args)
+    min_num = min(args)
+    sorted_numbers = deque(sorted(args))
+
+    missing_number = 0
+    clean_list = []
+    duplicated_list = []
+
+    while sorted_numbers:
+        first_num = sorted_numbers.popleft()
+        if first_num not in clean_list:
+            clean_list.append(first_num)
+        else:
+            if first_num not in duplicated_list:
+                duplicated_list.append(first_num)
+
+    for number in range(min_num, max_num + 1):
+        if number not in clean_list:
+            missing_number = number
+            break
+
+    return [missing_number, duplicated_list]
+
+
+print(numbers_searching(50, 50, 47, 47, 48, 45, 49, 44, 47, 45, 44, 44, 48, 44, 48))
