1027c918445abf1dc0419bddc6748977c3788651
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-13 23:25:33 +0200

    Update 3.Hourly_forecast.py

--- 0.Exams_Advanced/3.Hourly_forecast.py
+++ 0.Exams_Advanced/3.Hourly_forecast.py
@@ -42,3 +42,73 @@ def forecast(*args):
             rainy += f'{key} - {value}\n'
 
     return sunny + cloudy + rainy
+
+
+------------------------------------ Problem to resolve --------------------------------
+
+Write a function called forecast that stores information about the weather, and returns sorted 
+information for all locations. The function will receive a different number of arguments. The 
+arguments will be passed as tuples with two elements - the first one is the location, and the 
+second one is the weather:
+        Location name: string
+                        any string
+        Weather: string
+                "Sunny"
+                "Rainy"
+            "Cloudy"
+First, sort all locations by weather. First are positioned the locations with sunny weather, 
+next are the locations with cloudy weather, and last are the locations with rainy weather. For 
+each sequence of locations (e.g. all sunny locations), sort them by their name in ascending order 
+(alphabetically).
+In the end, return the output as described below.
+Note: Submit only the function in the judge system
+Input
+There will be no input from the console, just parameters passed to your function
+Output
+The output should look like this:
+"{first_sorted_location} - {weather}"
+"{second_sorted_location} - {weather}"
+…
+"{last_sorted_location} - {weather}"
+Constraints
+Each tuple given will always contain the location with its weather.
+You will never receive the same location twice or more times.
+-------------------------------------- Example inputs ----------------------------------
+Test Code
+print(forecast(
+    ("Sofia", "Sunny"),
+    ("London", "Cloudy"),
+    ("New York", "Sunny")))	
+Output    
+New York - Sunny
+Sofia - Sunny
+London - Cloudy
+---------------------------------
+Test Code
+print(forecast(
+    ("Beijing", "Sunny"),
+    ("Hong Kong", "Rainy"),
+    ("Tokyo", "Sunny"),
+    ("Sofia", "Cloudy"),
+    ("Peru", "Sunny"),
+    ("Florence", "Cloudy"),
+    ("Bourgas", "Sunny")))	
+---------------------------------
+Output    
+Beijing - Sunny
+Bourgas - Sunny
+Peru - Sunny
+Tokyo - Sunny
+Florence - Cloudy
+Sofia - Cloudy
+Hong Kong - Rainy
+----------------------------------
+Test Code
+print(forecast(
+    ("Tokyo", "Rainy"),
+    ("Sofia", "Rainy")))	
+Output      
+Sofia - Rainy
+Tokyo - Rainy
+
+"""
