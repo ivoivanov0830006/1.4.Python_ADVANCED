ce9b90a56f3e40b67a7a2082b4cab4479ea464d3
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-13 23:21:42 +0200

    Update 3.Magic_triangle.py

--- 0.Exams_Advanced/3.Magic_triangle.py
+++ 0.Exams_Advanced/3.Magic_triangle.py
@@ -13,3 +13,30 @@ def get_magic_triangle(rows):
 
 
 print(get_magic_triangle(5))
+
+
+"""
+------------------------------------ Problem to resolve --------------------------------
+
+Create a function called get_magic_triangle which will receive a single parameter (integer n) and it 
+should create a magic triangle which follows those rules:
+⦁	We start with this simple triangle [[1], [1, 1]]
+⦁	We generate the next rows until we reach n amount of rows
+⦁	Each number in each row is equal to the sum of the two numbers right above it in the triangle
+⦁	If the current number has no neighbor to the upper left/right, we just take the existing neighbor
+After you create the magic triangle, return it as a multidimensional list. Here is an example with n = 5
+ 
+Note: Submit only the function in the judge system
+Input
+⦁	There will be no inputs
+⦁	The function will be tested by passing different values of n
+⦁	You can test your function with the test code below
+Constraints
+⦁	N will be in range [2, 100]
+-------------------------------------- Example inputs ----------------------------------
+Test Code	
+get_magic_triangle(5)	
+Output
+[[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+
+"""
