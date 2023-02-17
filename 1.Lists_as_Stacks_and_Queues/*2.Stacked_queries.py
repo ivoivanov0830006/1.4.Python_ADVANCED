e7724a14db24ede7165a8a2124da156e63156039
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-17 20:12:49 +0200

    Update *2.Stacked_queries.py

--- 1.Lists_as_Stacks_and_Queues/*2.Stacked_queries.py
+++ 1.Lists_as_Stacks_and_Queues/*2.Stacked_queries.py
@@ -37,26 +37,6 @@ print(", ".join(list(map(str, final_stack))))
 
 
 """
-------------------------------------- Another Solution -----------------------------
-from collections import deque
-
-numbers = deque()
-map_functions = {
-    1: lambda x: numbers.append(x[1]),
-    2: lambda x: numbers.pop() if numbers else None,
-    3: lambda x: print(max(numbers)),
-    4: lambda x: print(min(numbers)),
-}
-
-for _ in range(int(input())):
-    number_data = [int(x) for x in input().split()]
-    map_functions[number_data[0]](number_data)
-
-numbers.reverse()
-
-print(*numbers, sep=", ")
-
-
 ------------------------------------- Problem to resolve ------------------------------
 
 You have an empty stack. You will receive an integer – N. On the next - N lines, you will receive
