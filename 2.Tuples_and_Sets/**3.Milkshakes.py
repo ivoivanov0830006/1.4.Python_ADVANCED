f1da4abcf7af03ed714e157f950a6e74c0a43290
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-17 20:31:46 +0200

    Update **3.Milkshakes.py

--- 2.Tuples_and_Sets/**3.Milkshakes.py
+++ 2.Tuples_and_Sets/**3.Milkshakes.py
@@ -41,3 +41,54 @@ if milk:
     print(f"Milk: {', '.join(map(str, milk))}")
 else:
     print("Milk: empty")
+
+    
+"""
+------------------------------------- Problem to resolve ------------------------------
+
+First, you will be given two sequences of integers representing chocolates and cups of milk.
+You have to start from the last chocolate and try to match it with the first cup of milk. 
+If their values are equal, you should make a milkshake and remove both ingredients. 
+Otherwise, you should move the cup of milk at the end of the sequence and decrease the value of 
+the chocolate by 5 without moving it from its position.
+If any of the values are equal to or below 0, you should remove them from the records right before 
+mixing them with the other ingredient.
+When you successfully prepare 5 chocolate milkshakes, or you have no more chocolate or cups of milk 
+left, you need to stop making chocolate milkshakes.
+Input
+On the first line of input, you will receive the integers representing the chocolate, separated by  ", ". 
+On the second line of input, you will receive the integers representing the cups of milk, separated by ", ".
+Output
+On the first line, print:
+If you successfully made 5 milkshakes: "Great! You made all the chocolate milkshakes needed!"
+Otherwise: "Not enough milkshakes."
+On the second line - print:
+If there are chocolates left: "Chocolate: {chocolate1}, {chocolate2}, (…)"
+Otherwise: "Chocolate: empty"
+On the third line - print:
+If there are cups of milk left: "Milk: {milk1}, {milk2}, {milk3}, (…)"
+Otherwise: "Milk: empty"
+Constraints
+All given args will be valid integers in the range [-100 … 100].
+-------------------------------------- Example inputs ----------------------------------
+Input	Output
+20, 24, -5, 17, 22, 60, 26
+26, 60, 22, 17, 24, 10, 55	
+Output
+Great! You made all the chocolate milkshakes needed!
+Chocolate: 20
+Milk: 10, 55
+--------------------------------------------------------
+Input	
+-10, -2, -30, 10
+-5	Not enough milkshakes.
+Chocolate: -10, -2, -30, 10
+Milk: empty
+2, 3, 3, 7, 2
+2, 7, 3, 3, 2, 14, 6	
+Output
+Great! You made all the chocolate milkshakes needed!
+Chocolate: empty
+Milk: 14, 6
+
+"""
