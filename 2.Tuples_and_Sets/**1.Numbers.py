750df7c333833ed1717cf0115ceb667ab7d134c7
Ivaylo Ivanov <108527062+ivoivanov0830006@users.noreply.github.com>
2023-02-17 20:20:35 +0200

    Update **1.Numbers.py

--- 2.Tuples_and_Sets/**1.Numbers.py
+++ 2.Tuples_and_Sets/**1.Numbers.py
@@ -44,3 +44,47 @@ for _ in range(number_lines):
 
 print(*sorted(first_sequence), sep=", ")
 print(*sorted(second_sequence), sep=", ")
+
+
+"""
+------------------------------------- Problem to resolve ------------------------------
+
+First, you will be given two sequences of integers values on different lines. The values of the 
+sequences are separated by a single space between them.
+Keep in mind that each sequence should contain only unique values.
+Next, you will receive a number - N. On the next N lines, you will receive one of the following commands:
+    "Add First {args, separated by a space}" - add the given args at the end of the first sequence of args.
+    "Add Second {args, separated by a space}" - add the given args at the end of the second sequence of args.
+    "Remove First {args, separated by a space}" - remove only the args contained in the first sequence.
+    "Remove Second {args, separated by a space}" - remove only the args contained in the second sequence.
+    "Check Subset" - check if any of the given sequences are a subset of the other. If it is, print "True". 
+Otherwise, print "False".
+In the end, print the final sequences, separated by a comma and a space ", ". The values in each sequence 
+should be sorted in ascending order.
+-------------------------------------- Example inputs ----------------------------------
+Input	
+1 2 3 4 5
+1 2 3
+3
+Add First 5 6
+Remove Second 8 9 11
+Check Subset	
+Output
+True
+1, 2, 3, 4, 5, 6
+1, 2, 3
+--------------------------
+Input	
+5 4 2 9 9 5 4
+1 1 1 5 6 5
+4
+Add First 5 6 9 3
+Add Second 1 2 3 3 3
+Check Subset
+Remove Second 1 2 3 4 5	
+Output
+False
+2, 3, 4, 5, 6, 9
+6
+
+"""
